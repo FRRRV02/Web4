bd25f27c17e59d9e833d05501aa4ed90498bb7d8
MauliaAstuti <maulia10222052@sttcipasung.ac.id>
2025-01-08 12:20:13 +0700

    menambahkan navbar

---
+++ resources/views/header/index.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar">
+        <div class="logo">
+            <img src="public/external/sttc.png" alt="Logo Kampus" class="logo-img">
+            
+        </div>
+        <div class="menu-toggle" onclick="toggleMenu()">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <ul>
+            <li><a href="#">Beranda</a></li>
+            <li><a href="#">Tentang</a></li>
+            <li><a href="#">Program Studi</a></li>
+            <li><a href="#">Berita</a></li>
+            <li><a href="#">Kontak</a></li>
+        </ul>
+    </nav>

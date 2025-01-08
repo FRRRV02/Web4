eb552c23e3cdb5c67f846f9b37ada382e19d41b1
MauliaAstuti <maulia10222052@sttcipasung.ac.id>
2025-01-08 16:55:59 +0700

    menambahkan home

--- resources/views/footer/index.blade.php
+++ resources/views/footer/index.blade.php
@@ -5,17 +5,17 @@
             <img class="footer-img" src="{{url('storage/img/logo_sttc-removebg-preview.png')}}" alt="">
             </div>
             <div class="footer-address">
-                <p>JL. Cisinga NO.KM1, Cilampunghilir</p>
-                <p>Kec. Padakembang, Kabupaten Tasikmalaya</p>
-                <p>Jawa Barat 46466</p>
+                <p class="pp">JL. Cisinga NO.KM1, Cilampunghilir</p>
+                <p class="pp">Kec. Padakembang, Kabupaten Tasikmalaya</p>
+                <p class="pp">Jawa Barat 46466</p>
             </div>
             <div class="footer-social">
                 <a href="#"><i class="fab fa-facebook"></i></a>
                 <a href="#"><i class="fab fa-instagram"></i></a>
                 <a href="#"><i class="fab fa-twitter"></i></a>
                 <a href="#"><i class="fab fa-youtube"></i></a>
-                <p>Email: <a href="mailto:admin@sttcipasung.ac.id">admin@sttcipasung.ac.id</a></p>
-                <p>Telp: (0265) 2550424</p>
+                <p class="pp">Email: <a href="mailto:admin@sttcipasung.ac.id">admin@sttcipasung.ac.id</a></p>
+                <p class="pp">Telp: (0265) 2550424</p>
             </div>
         </div>
     </footer>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,3 +1,44 @@
  <x-layout>
-    <h1>home</h1>
+ <div class="hero-beranda">
+        <img class="hero-beranda" src="gambar/tq_6om4mzp9xq-eof-800h.png" alt="">
+    </div>
+
+    <div class="content-beranda">
+        <div class="section-beranda">
+            <h2 class="judul">STT CIPASUNG</h2>
+            <p class="p">Didirikan atas prakarsa Drs. KH. Abdul Chobir, MT. dibawah Yayasan Pesantren Cipasung, Tasikmalaya.
+                Sekolah Tinggi Teknologi Cipasung (STT Cipasung) merupakan lembaga pendidikan tinggi formal di bawah Yayasan Pondok Pesantren Cipasung. Pesantren Cipasung didirikan tahun 1931 oleh almarhum KH. Ruhiat, terletak di Desa Cipakat Kecamatan Singaparna Kabupaten Tasikmalaya, sekitar 100 km dari Kota Bandung. 
+                <br>
+                <br>
+                Dalam Perkembangan selanjutnya setelah KH. Ruhiat wafat (1931-1977), perjuangan beliau diteruskan oleh KH. Moh. Ilyas Ruhiat (1977-2007), yang pernah menjabat sebagai Rais Aam PBNU (1994-1999), KH Dudung Abdul Halim, MA. (2007-2012), dan sekarang dilanjutkan oleh KH. Drs. A. Bunyamin Ruhiat, M.S.I.
+                <br>
+                <br>
+                STT Cipasung didirikan pada tahun 1997, melengkapi lembaga-lembaga pendidikan yang sudah ada di lingkungan Pondok Pesantren Cipasung, mulai dari Paud, TK, MI, SMPI, MTs, SMAI, MAN, dan IAIC.
+                <br>
+                <br>
+                Pemrakarsa utama berdirinya Sekolah Tinggi Teknologi Cipasung adalah Drs. H. Abdul Chobir, MT atas anjuran Pimpinan Pondok Pesantren Cipasung pada masa itu, yaitu KH. Moh. Ilyas Ruhiat. Pendirian kampus ini, juga atas bantuan dan arahan dari tim Institut Teknologi Bandung, serta dorongan dari Prof. Dr. Ginanjar Kartasasmita dan Ir. Fadel Muhammad ketika melakukan kunjungan ke Pondok Pesantren Cipasung.</p>
+            <a href="#" class="button">Pelajari Lebih Lanjut</a>
+        </div>
+
+        <div class="section-beranda">
+            <h2 class="judul">fasilitas</h2>
+            <div class="grid-beranda">
+                <div class="card-beranda">
+                    <img class="card-img" src="gambar/tq_6om4mzp9xq-eof-800h.png" alt="Program Studi 1">
+                    <h3 class="h33">Teknik Informatika</h3>
+                    <p class="p">Mengembangkan teknologi dan solusi inovatif untuk era digital.</p>
+                </div>
+                <div class="card-beranda">
+                    <img class="card-img" src="gambar/tq_6om4mzp9xq-eof-800h.png" alt="Program Studi 2">
+                    <h3 class="h33">Manajemen</h3>
+                    <p class="p">Belajar mengelola sumber daya dengan efektif dan efisien.</p>
+                </div>
+                <div class="card-beranda">
+                    <img class="card-img" src="gambar/tq_6om4mzp9xq-eof-800h.png" alt="Program Studi 3">
+                    <h3 class="h33">Desain Komunikasi Visual</h3>
+                    <p class="p">Ekspresikan kreativitas melalui desain yang berpengaruh.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-layout>
